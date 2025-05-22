ab7beec1f3d60d8af49c8425dd5c99573cd784b9
Felipe Toshio <felipetoshio1214@gmail.com>
2025-05-22 20:55:42 -0300

    adding the kubernetes and the terraform infra

---
+++ infra/main.tf
@@ -0,0 +1,44 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+provider "google-beta" {
+  project = var.project_id
+  region  = var.region
+}
+
+resource "google_container_cluster" "primary" {
+  name               = "quiosque-food-cluster"
+  location           = var.region
+  remove_default_node_pool = true
+  initial_node_count = 1
+}
+
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "primary-node-pool"
+  location   = var.region
+  cluster    = google_container_cluster.primary.name
+  node_count = 2
+
+  node_config {
+    machine_type = "e2-medium"
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+}
+
+provider "kubernetes" {
+  host                   = google_container_cluster.primary.endpoint
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
+}
+
+data "google_client_config" "default" {}
+
+resource "kubernetes_manifest" "app_manifests" {
+  for_each = fileset("${path.module}/kubernetes", "*.yaml")
+
+  manifest = yamldecode(file("${path.module}/kubernetes/${each.value}"))
+}
