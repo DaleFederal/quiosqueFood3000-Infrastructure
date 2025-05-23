1fc97d051808093b8c62542db30897a2eed15dbd
FelipeToshio1 <42159484+FelipeToshio1@users.noreply.github.com>
2025-05-22 21:33:26 -0300

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -23,6 +23,8 @@ resource "google_container_node_pool" "primary_nodes" {
 
   node_config {
     machine_type = "e2-medium"
+    disk_type    = "pd-ssd"
+    disk_size_gb = 100
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
     ]
